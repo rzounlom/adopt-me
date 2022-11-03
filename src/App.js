535d9f3cefe72e33dc9276dcef7b233e2c4a02f3
Romaric Zounlome <rzounlom@gmail.com>
2022-11-02 20:22:27 -0400

    add custom hook; complete basic functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 import { render } from "react-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mix" />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
