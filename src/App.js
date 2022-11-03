85ec9f3eeb340b6e2be5b231f4548e09683aeb8b
Romaric Zounlome <rzounlom@gmail.com>
2022-11-02 20:46:06 -0400

    complete react-router setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import Details from "./Details";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 import { StrictMode } from "react";
@@ -6,10 +9,17 @@ import { render } from "react-dom";
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1>Adopt Me</h1>
-        <SearchParams />
-      </div>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        ;
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+      ;
     </StrictMode>
   );
 };
