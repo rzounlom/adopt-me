c89cf5d20494d2a804def39b442bf92d1ebafeeb
Romaric Zounlome <rzounlom@gmail.com>
2022-11-07 09:11:55 -0500

    complete Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,6 +10,14 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  // add event listener
+  handleIndexClick = (event) => {
+    this.setState({
+      //the + sign in hte front converts string index to number
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -20,8 +28,10 @@ class Carousel extends Component {
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo}
               src={photo}
+              data-index={index}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
